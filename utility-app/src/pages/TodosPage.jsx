10659cb8776e96d3499680843e85b09fce36011a
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-11-22 12:53:02 +0530

    added delete functionality for todos

--- utility-app/src/pages/TodosPage.jsx
+++ utility-app/src/pages/TodosPage.jsx
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { todoSchema } from "../schema";
-
+import { useState } from "react";
+import { nanoid } from "nanoid";
 const TodosPage = () => {
   const {
     register,
@@ -9,7 +10,21 @@ const TodosPage = () => {
     formState: { errors },
   } = useForm({ resolver: yupResolver(todoSchema) });
 
-  const onSubmit = (data) => console.log(data);
+  const [todos, setTodos] = useState([]);
+
+  const onSubmit = (data) => {
+    setTodos((prevState) => [...prevState, data.todoItem]);
+  };
+
+  const handleDelete = (todoItem) => {
+    console.log("TODO TO BE DELETED: ", todoItem);
+    const filteredTodos = todos.filter((todo) => todo !== todoItem);
+    setTodos(filteredTodos);
+  };
+
+  const handleEdit = (todoItem) => {
+    console.log("TODO TO BE EDITED: ", todoItem);
+  };
 
   return (
     <div className="p-4 grid grid-cols-4 gap-4">
@@ -33,7 +48,36 @@ const TodosPage = () => {
       </div>
 
       <div className="p-4 bg-base-200 col-span-4 rounded-xl">
-        <h1 className="text-center">Nothing to show here</h1>
+        {todos.length === 0 ? (
+          <h1 className="text-center">Nothing to show here</h1>
+        ) : (
+          <ul className="grid gap-4">
+            {todos.map((todo) => (
+              <li key={nanoid()} className="collapse bg-base-200">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-base-300 peer-checked:text-base-content">
+                  {todo}
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-base-300 peer-checked:text-secondary-content">
+                  <div className="flex gap-4">
+                    <button
+                      onClick={() => handleDelete(todo)}
+                      className="btn btn-error btn-outline"
+                    >
+                      delete
+                    </button>
+                    <button
+                      onClick={() => handleEdit(todo)}
+                      className="btn btn-primary btn-outline"
+                    >
+                      edit
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
