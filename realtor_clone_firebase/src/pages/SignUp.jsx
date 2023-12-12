856ad68bb386814b6f1782eeb747cc319f6aefa6
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-12-12 11:40:14 +0530

    added forger password, sign up and sign in pages

--- realtor_clone_firebase/src/pages/SignUp.jsx
+++ realtor_clone_firebase/src/pages/SignUp.jsx
@@ -1,5 +1,112 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = formData;
+
+  const onChange = (e) => {
+    return setFormData((prevSate) => ({
+      ...prevSate,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  return (
+    <section>
+      <h1 className="text-center text-3xl mt-6 font-bold">Sign Up</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto ">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            className="w-full rounded-xl"
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="key"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
+              type="text"
+              name=""
+              id="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Full Name"
+            />
+            <input
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
+              type="email"
+              name=""
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email Address"
+            />
+            <div className="relative mb-6">
+              <input
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+                type={showPassword ? "text" : "password"}
+                name=""
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+              />
+              {showPassword ? (
+                <FaEyeSlash
+                  className="cursor-pointer absolute right-3 top-3 text-xl "
+                  onClick={() => setShowPassword(false)}
+                />
+              ) : (
+                <FaEye
+                  className="cursor-pointer absolute right-3 top-3 text-xl"
+                  onClick={() => setShowPassword(true)}
+                />
+              )}
+            </div>
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">
+                Have an account?
+                <Link
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                  to="/sign-in"
+                >
+                  Sign In
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/forgot-password"
+                  className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out"
+                >
+                  Forgot Password?
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg active:bg-blue-800 transition duration-150 ease-in-out"
+            >
+              Sign up
+            </button>
+            <div className="flex my-4 before:border-t  before:flex-1 items-center before:border-gray-300 after:border-t  after:flex-1  after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SignUp;
